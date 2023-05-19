9e04261a8fb26e192a2a6b78be48dfdc85ba13ca
subhajit51193 <nnorth87@gmail.com>
2023-05-19 20:08:38 +0530

    working on POST method

--- RestaurantListingApp/src/main/java/com/app/service/RestaurantServiceImpl.java
+++ RestaurantListingApp/src/main/java/com/app/service/RestaurantServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.exception.RestaurantException;
+import com.app.model.Cuisin;
 import com.app.model.Location;
 import com.app.model.Restaurant;
+import com.app.repository.CuisinRepository;
 import com.app.repository.LocationRepository;
 import com.app.repository.RestaurantRepository;
 
@@ -20,6 +22,9 @@ public class RestaurantServiceImpl implements RestaurantService{
 	@Autowired
 	private LocationRepository locationRepository;
 	
+	@Autowired
+	private CuisinRepository cuisinRepository;
+	
 	@Override
 	public Restaurant addRestaurant(Restaurant restaurant) throws RestaurantException {
 		
@@ -28,6 +33,12 @@ public class RestaurantServiceImpl implements RestaurantService{
 			location.getRestaurants().add(restaurant);
 			locationRepository.save(location);
 			restaurant.getLocations().add(location);
+		}
+		Set<Cuisin> cuisins = restaurant.getCuisins();
+		for (Cuisin cuisin : cuisins) {
+			cuisin.getRestaurants().add(restaurant);
+			cuisinRepository.save(cuisin);
+			restaurant.getCuisins().add(cuisin);
 		}
 		return restaurantRepository.save(restaurant);
 		
